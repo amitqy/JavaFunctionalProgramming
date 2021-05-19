776c6dbe131a8f04a3e670a8255922dec7d74b44
amitqy <padaliya.amit@gmail.com>
2021-05-19 16:46:39 +0530

    added how to chain functions

--- src/main/java/functionalInterface/_Function.java
+++ src/main/java/functionalInterface/_Function.java
@@ -11,13 +11,24 @@ public class _Function {
         Integer increment2 = incrementByOne.apply(1);
         System.out.println(increment2);
 
+        // new way of combining
+        Function<Integer, Integer> incrementByOneAndMultiplyBy10 = incrementByOne.andThen(multiplyByTen);
+        System.out.println(incrementByOneAndMultiplyBy10.apply(1));
+
+
 
     }
     // normal function in java
+
     static int increment(int number){
         return number+1;
     }
     // Function<Input Type,Return Type>
     static Function<Integer,Integer> incrementByOne = number-> ++number;
+
+    // we want to merge many functions to do one work
+    // after incrementing by 1, we want to apply this function immediately
+    // see line 14
+    static Function<Integer,Integer> multiplyByTen = number->number*10;
 
 }
